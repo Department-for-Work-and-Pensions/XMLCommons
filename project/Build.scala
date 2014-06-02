20a5c9b54c512e85f37447093e2ae73b9afb5b42
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-06-02 16:33:46 +0100

    DE519:Made CareBreak and PeriodAbroad maxOccurs to unbounded

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object ApplicationBuild extends Build {
   val appName = "xmlCommons"
 
-  val appVersion = "1.1"
+  val appVersion = "1.2"
 
   val appDependencies = Seq(
     libraryDependencies += "org.specs2" %% "specs2" % "2.3.6" % "test",
