7afe55680a1d5283c4800d5233a84bd9a756b123
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-08-13 10:40:47 +0100

    Upgraded version as from integration-pilot merge.

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object ApplicationBuild extends Build {
   val appName = "xmlCommons"
 
-  val appVersion = "2.0.9p"
+  val appVersion = "2.0.10"
 
   val appDependencies = Seq(
     libraryDependencies += "org.specs2" %% "specs2" % "2.3.6" % "test",
