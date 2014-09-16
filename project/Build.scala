22218ed73ce80b33eaf70d163d0d4831d899dc19
Jorge Migueis <jorge.migueis@valtech.co.uk>
2014-09-16 11:52:04 +0100

    US903 - Use new version of carers common.

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object ApplicationBuild extends Build {
   val appName = "xmlCommons"
 
-  val appVersion = "3.0.2"
+  val appVersion = "3.2.0"
 
   val appDependencies = Seq(
     libraryDependencies += "org.specs2" %% "specs2" % "2.3.6" % "test",
@@ -19,7 +19,7 @@ object ApplicationBuild extends Build {
     libraryDependencies += "junit" % "junit" % "4.11" % "test",
     libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test->default",
     libraryDependencies += "org.jasypt" % "jasypt" % "1.9.1" classifier "lite",
-    libraryDependencies += "com.dwp.carers" %% "carerscommon" % "5.4"
+    libraryDependencies += "com.dwp.carers" %% "carerscommon" % "5.5"
     // Add your project dependencies here,
   )
 
