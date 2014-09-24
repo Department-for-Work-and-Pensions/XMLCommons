eb91023964b7901375cfe1d41c629d372625a4cc
PrafullaC <prafulla.chandra@valtech.co.uk>
2014-09-24 16:40:55 +0100

    US927 - Fixing tests

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object ApplicationBuild extends Build {
   val appName = "xmlCommons"
 
-  val appVersion = "3.2.0"
+  val appVersion = "3.2.1"
 
   val appDependencies = Seq(
     libraryDependencies += "org.specs2" %% "specs2" % "2.3.6" % "test",
