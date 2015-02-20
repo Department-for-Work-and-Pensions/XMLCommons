ce582363dc19bf2b7e95c8159b4e9061ab93025e
PrafullaC <prafulla.chandra@valtech.co.uk>
2015-02-20 15:25:22 +0000

    US1119 - Removed 'ArmedForcesIndependencePayment'

---
+++ src/test/java/com/dwp/carers/s2/xml/validation/v9/CaFutureXmlValidatorImpl2Test.java
@@ -0,0 +1,54 @@
+package com.dwp.carers.s2.xml.validation.v9;
+
+import com.dwp.carers.s2.xml.XmlTestBase;
+import com.dwp.carers.s2.xml.validation.XmlValidator;
+import com.dwp.carers.s2.xml.validation.XmlValidatorFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CaFutureXmlValidatorImpl2Test extends XmlTestBase {
+
+    private XmlValidator validator;
+    private String version = "0.9";
+
+
+    @Before
+    public void setUp() {
+        validator = XmlValidatorFactory.buildCaFutureValidator();
+    }
+
+    @Test
+    public void testValidateSuccessFullWithOnlyMandatoryFields() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerClaimGeneratedFromXML1.xml", version));
+        assertTrue(validator.validate(xml));
+    }
+
+    @Test
+    public void testValidateSuccessFullWithAllFields() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerClaimGeneratedFromXMLWithOptionals1.xml", version));
+        assertTrue(validator.validate(xml));
+    }
+
+    @Test
+    public void testValidateFailWithEmptyXml() throws Exception {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerClaimEmptyXMLWithVersion.xml", version));
+        assertFalse(validator.validate(xml));
+    }
+
+    @Test
+    public void testValidateFailWithUnkownXmlVersion() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerClaimGeneratedFromXMLUnknownVersion.xml", version));
+        assertFalse(validator.validate(xml));
+    }
+
+    @Test
+    public void testValidateFailWithInvalidLanguage() throws Exception {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerClaimGeneratedFromXMLWithInvalidLanguage.xml", version));
+        assertFalse(validator.validate(xml));
+    }
+}
---
+++ src/test/java/com/dwp/carers/s2/xml/validation/v9/CocFutureXmlValidatorImplTest.java
@@ -0,0 +1,41 @@
+package com.dwp.carers.s2.xml.validation.v9;
+
+import com.dwp.carers.s2.xml.XmlTestBase;
+import com.dwp.carers.s2.xml.validation.XmlValidator;
+import com.dwp.carers.s2.xml.validation.XmlValidatorFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CocFutureXmlValidatorImplTest extends XmlTestBase {
+
+    private XmlValidator validator;
+    private String version = "0.9";
+
+    @Before
+    public void setUp() throws Exception {
+        validator = XmlValidatorFactory.buildCocFutureValidator();
+    }
+
+
+    @Test
+    public void testValidateFailWithEmptyXml()  {
+        assertFalse(validator.validate(""));
+    }
+
+    @Test
+    public void testValidateSuccessFullWithOnlyMandatoryFields() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerCOCWithMandatoryValues.xml",version));
+        assertTrue(validator.validate(xml));
+    }
+
+    @Test
+    public void testValidateFailWithUnkownXmlVersion() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerCOCWithMandatoryValuesWithUnknownVersion.xml", version));
+        assertFalse(validator.validate(xml));
+    }
+}
