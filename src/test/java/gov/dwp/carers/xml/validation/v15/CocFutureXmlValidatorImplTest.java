d81757ef5c205640912b4883ae72136672903945
Tudor Malene <tudor.malene@gmail.com>
2015-09-16 10:31:50 +0100

    CADS-47 - added EmployerName to schema

---
+++ src/test/java/gov/dwp/carers/xml/validation/v15/CocFutureXmlValidatorImplTest.java
@@ -0,0 +1,41 @@
+package gov.dwp.carers.xml.validation.v15;
+
+import gov.dwp.carers.xml.XmlTestBase;
+import gov.dwp.carers.xml.validation.XmlValidator;
+import gov.dwp.carers.xml.validation.XmlValidatorFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CocFutureXmlValidatorImplTest extends XmlTestBase {
+
+    private XmlValidator validator;
+    private String version = "0.15";
+
+    @Before
+    public void setUp() throws Exception {
+        validator = XmlValidatorFactory.buildCocFutureValidator();
+    }
+
+
+    @Test
+    public void testValidateFailWithEmptyXml()  {
+        assertFalse(validator.validate(""));
+    }
+
+    @Test
+    public void testValidateSuccessFullWithOnlyMandatoryFields() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerCOCWithMandatoryValues.xml",version));
+        assertTrue(validator.validate(xml));
+    }
+
+    @Test
+    public void testValidateFailWithUnkownXmlVersion() throws IOException {
+        final String xml = readXMLFile(String.format("future/%s/DWPCarerCOCWithMandatoryValuesWithUnknownVersion.xml", version));
+        assertFalse(validator.validate(xml));
+    }
+}
