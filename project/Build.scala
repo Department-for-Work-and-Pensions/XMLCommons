1cf5e9a56579f2c3da542c9270982bcf2ef6b929
Peter Whitehead <peter@scrumconnect.com>
2015-12-09 13:35:54 +0000

    CADS-413 : NISSA: Add support for origin tag in C3 (xml-generated)

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object ApplicationBuild extends Build {
   val appName = "xmlCommons"
 
-  val appVersion = "7.0"
+  val appVersion = "7.1"
 
   val appDependencies = Seq(
     libraryDependencies += "org.apache.santuario" % "xmlsec" % "1.4.8",
