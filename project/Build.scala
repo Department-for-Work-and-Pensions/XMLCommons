52e797c5f814de82dee59094c68ae708a7ff2902
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-04-07 17:26:08 +0100

    CADS-795 : Incomes xsd. Up xmlcommons to 7.18-SNAPSHOT. Include OtherPaymentQuestion.

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object ApplicationBuild extends Build {
   val appName = "xmlCommons"
 
-  val appVersion = "7.17-SNAPSHOT"
+  val appVersion = "7.18-SNAPSHOT"
 
   val appDependencies = Seq(
     libraryDependencies += "org.specs2" %% "specs2-core" % "3.3.1" % "test" withSources() withJavadoc(),
